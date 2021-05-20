cd37413571ccd9e841a9deaeffbf62f0b410e641
Subhajit Sahu <wolfram77@gmail.com>
2021-05-20 06:47:57 +0530

    Initial commit

---
+++ src/main.hxx
@@ -0,0 +1,10 @@
+#pragma once
+#include "_main.hxx"
+#include "DiGraph.hxx"
+#include "vertices.hxx"
+#include "edges.hxx"
+#include "transpose.hxx"
+#include "snap.hxx"
+#include "ranks.hxx"
+#include "pagerank.hxx"
+#include "pagerankSeq.hxx"
